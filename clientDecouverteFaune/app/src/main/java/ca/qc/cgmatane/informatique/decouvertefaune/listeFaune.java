425d97ae7693a3284fee0111ac193e6fce390313
Bassirou <haidara.bassirou@yahoo.com>
2018-09-25 14:11:42 -0400

    Auto stash before merge of "master" and "origin/master"
    vue liste faune

---
+++ clientDecouverteFaune/app/src/main/java/ca/qc/cgmatane/informatique/decouvertefaune/listeFaune.java
@@ -0,0 +1,13 @@
+package ca.qc.cgmatane.informatique.decouvertefaune;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class listeFaune extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_liste_faune);
+    }
+}
