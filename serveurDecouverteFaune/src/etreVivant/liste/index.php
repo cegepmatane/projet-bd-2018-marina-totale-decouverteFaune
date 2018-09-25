24a15c9a5ee6c27e1b4341d498968a59f8be3778
Florian <flolevy33@gmail.com>
2018-09-25 00:58:59 -0400

    #8 appel fonction listerEtreVivant

--- serveurDecouverteFaune/src/etreVivant/liste/index.php
+++ serveurDecouverteFaune/src/etreVivant/liste/index.php
@@ -2,6 +2,8 @@
 
 	include "../../accesseur/PenseeDAO.php";
 	$etreVivantDAO = new EtreVivantDAO();
-	print_r($etreVivantDAO);
+	//print_r($etreVivantDAO);
+	$listeEtreVivant = $etreVivantDAO->listerEtreVivant();
+	print_r($listePensees);
 	
 ?><?php 
