d7fefb809a3f7bc3b7efd80481d2414e63f50590
Florian <flolevy33@gmail.com>
2018-09-25 01:07:55 -0400

    #8 liste etreVivant XML

--- serveurDecouverteFaune/src/etreVivant/liste/index.php
+++ serveurDecouverteFaune/src/etreVivant/liste/index.php
@@ -1,9 +1,34 @@
 <?php 
 
-	include "../../accesseur/PenseeDAO.php";
+	include "../../accesseur/EtreVivantDAO.php";
 	$etreVivantDAO = new EtreVivantDAO();
 	//print_r($etreVivantDAO);
 	$listeEtreVivant = $etreVivantDAO->listerEtreVivant();
-	print_r($listePensees);
+	//print_r($listePensees);
 	
-?><?php 
+?>
+<?php
+
+header("Content-type: text/xml");
+echo '<?xml version="1.0" encoding="UTF-8"?>';
+?>
+<etreVivants>
+
+<?php
+
+foreach($listeEtreVivant as $etreVivant)
+{
+	?>
+	<etreVivant>
+		<categorie><?=($etreVivant->categorie)?></categorie>
+		<urlwiki><?=($etreVivant->urlwiki)?></urlwiki>
+		<urlimage><?=$etreVivant->urlimage?></urlimage>
+		<espece><?=$etreVivant->espece?></espece>
+		<id><?=$etreVivant->id?></id>
+	</etreVivant>
+	<?php
+}
+?>
+</etreVivants>
+
+
