bb732cdc14e5e2ded3722af8b2964dcbc36c6eae
Bassirou <haidara.bassirou@yahoo.com>
2018-09-28 14:11:42 -0400

    refactor

--- clientDecouverteFaune/app/src/main/java/ca/qc/cgmatane/informatique/decouvertefaune/decouverteFaune.java
+++ clientDecouverteFaune/app/src/main/java/ca/qc/cgmatane/informatique/decouvertefaune/decouverteFaune.java
@@ -3,11 +3,11 @@ package ca.qc.cgmatane.informatique.decouvertefaune;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class listeFaune extends AppCompatActivity {
+public class decouverteFaune extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.vue_liste_faune);
+        setContentView(R.layout.vue_decouverte_faune);
     }
 }
