4ddaff544d08a402deebb68016d02dae580edc08
Bassirou <haidara.bassirou@yahoo.com>
2018-09-28 14:09:40 -0400

    refactor des noms

--- clientDecouverteFaune/app/src/main/java/ca/qc/cgmatane/informatique/decouvertefaune/MainActivity.java
+++ clientDecouverteFaune/app/src/main/java/ca/qc/cgmatane/informatique/decouvertefaune/MainActivity.java
@@ -3,11 +3,11 @@ package ca.qc.cgmatane.informatique.decouvertefaune;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class decouverteFaune extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.vue_accueil);
+        setContentView(R.layout.activity_main);
     }
 }
