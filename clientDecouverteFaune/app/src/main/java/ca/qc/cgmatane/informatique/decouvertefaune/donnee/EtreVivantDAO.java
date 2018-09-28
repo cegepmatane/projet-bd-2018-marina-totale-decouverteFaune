e75b2993a6fb256735bf94aa2fb212bd156ee73b
Florian <flolevy33@gmail.com>
2018-09-28 14:57:08 -0400

    #4 listeVivant URL DAO etrevivant Java

--- clientDecouverteFaune/app/src/main/java/ca/qc/cgmatane/informatique/decouvertefaune/donnee/EtreVivantDAO.java
+++ clientDecouverteFaune/app/src/main/java/ca/qc/cgmatane/informatique/decouvertefaune/donnee/EtreVivantDAO.java
@@ -1,4 +1,35 @@
 package ca.qc.cgmatane.informatique.decouvertefaune.donnee;
 
-public class EtreVivantDAO {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+import ca.qc.cgmatane.informatique.decouvertefaune.modele.EtreVivant;
+
+public class EtreVivantDAO<T> {
+
+    List<EtreVivant> listeEtreVivant;
+
+    public EtreVivantDAO(){
+        this.listeEtreVivant = new ArrayList<>();
+    }
+
+
+    public List<EtreVivant> getListeEtreVivant() {
+
+        String xmlRssEtreVivant = "";
+        try{
+            URL urlRssEtreVivant = new URL("http://158.69.113.110/serveurDecouverteFaune/src/etreVivant/liste/index.php");
+
+            BufferedReader influx = new BufferedReader(new InputStreamReader(urlRssEtreVivant.openStream()));
+
+        }
+        catch (MalformedURLException e){
+            e.printStackTrace();
+        }
+        return listeEtreVivant;
+    }
 }
