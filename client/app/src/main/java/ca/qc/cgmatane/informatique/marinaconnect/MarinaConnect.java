9603ee70c7e8b926b08a53047c755a53750f4911
Florian <flolevy33@gmail.com>
2018-10-02 14:40:24 -0400

    Create client new

---
+++ client/app/src/main/java/ca/qc/cgmatane/informatique/marinaconnect/MarinaConnect.java
@@ -0,0 +1,13 @@
+package ca.qc.cgmatane.informatique.marinaconnect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MarinaConnect extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_marina_connect);
+    }
+}
