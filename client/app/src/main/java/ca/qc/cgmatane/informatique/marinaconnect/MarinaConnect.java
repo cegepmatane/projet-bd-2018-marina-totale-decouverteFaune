3903dbac7b9395c1252c04b42d89184790f80552
Florian <flolevy33@gmail.com>
2018-10-08 17:19:36 -0400

    FIX connection HTTP asynctask

--- client/app/src/main/java/ca/qc/cgmatane/informatique/marinaconnect/MarinaConnect.java
+++ client/app/src/main/java/ca/qc/cgmatane/informatique/marinaconnect/MarinaConnect.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import ca.qc.cgmatane.informatique.marinaconnect.donnee.UtilisateurDAO;
 import ca.qc.cgmatane.informatique.marinaconnect.vue.VueConnection;
 import ca.qc.cgmatane.informatique.marinaconnect.vue.VueInscription;
 
@@ -13,6 +14,8 @@ public class MarinaConnect extends AppCompatActivity {
 
     static final public int ACTIVITE_SE_CONNECTER = 1;
     static final public int ACTIVITE_CREER_COMPTE = 2;
+    protected UtilisateurDAO utilisateurDAO;
+
 
     protected Intent intentionNaviguerSeConnecter;
     protected Intent intentionNaviguerCreerCompte;
@@ -21,6 +24,8 @@ public class MarinaConnect extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vue_accueil);
+        utilisateurDAO = utilisateurDAO.getInstance();
+
 
         intentionNaviguerSeConnecter = new Intent(this, VueConnection.class);
         Button actionNaviguerSeConnecter = (Button) findViewById(R.id.action_naviguer_connection);
--- client/app/src/main/java/ca/qc/cgmatane/informatique/marinaconnect/donnee/UtilisateurDAO.java
+++ client/app/src/main/java/ca/qc/cgmatane/informatique/marinaconnect/donnee/UtilisateurDAO.java
@@ -6,6 +6,7 @@ import java.util.concurrent.ExecutionException;
 import ca.qc.cgmatane.informatique.marinaconnect.modele.Utilisateur;
 
 public class UtilisateurDAO {
+
     private static UtilisateurDAO instance = null;
 
     public static UtilisateurDAO getInstance() {
@@ -20,7 +21,10 @@ public class UtilisateurDAO {
         public void ajouterUtilisateurSQL(Utilisateur utilisateur){
 
             try{
-                String url = "http://158.69.113.110/findItServeur/utilisateur/ajouter/index.php?mail="+utilisateur.getMail()+"&mdp="+utilisateur.getMdp();
+                System.out.println(utilisateur.getMail());
+                System.out.println(utilisateur.getMdp());
+
+                String url = "http://158.69.113.110/serveurDecouverteFaune/src/Utilisateur/ajouter/index.php?mail=test&mdp=test";
 
                 String resultat;
                 HttpGetRequete getRequete = new HttpGetRequete();
