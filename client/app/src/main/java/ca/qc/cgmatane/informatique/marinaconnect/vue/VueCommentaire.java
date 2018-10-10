3bc979d5dcd3594aa2911af9268da8e8b13546b6
Bassirou <haidara.bassirou@yahoo.com>
2018-10-09 21:54:36 -0400

    #10 Creation vue commentaire

---
+++ client/app/src/main/java/ca/qc/cgmatane/informatique/marinaconnect/vue/VueCommentaire.java
@@ -0,0 +1,15 @@
+package ca.qc.cgmatane.informatique.marinaconnect.vue;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ca.qc.cgmatane.informatique.marinaconnect.R;
+
+public class VueCommentaire extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_commentaire);
+    }
+}
