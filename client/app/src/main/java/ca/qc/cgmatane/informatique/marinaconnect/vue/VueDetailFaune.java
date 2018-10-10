88291f6419790fee9ff889e53ff7a3c45f438de3
Florian <flolevy33@gmail.com>
2018-10-09 22:10:14 -0400

    #10 vue

--- client/app/src/main/java/ca/qc/cgmatane/informatique/marinaconnect/vue/VueDetailFaune.java
+++ client/app/src/main/java/ca/qc/cgmatane/informatique/marinaconnect/vue/VueDetailFaune.java
@@ -1,6 +1,22 @@
 package ca.qc.cgmatane.informatique.marinaconnect.vue;
 
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import ca.qc.cgmatane.informatique.marinaconnect.R;
+import ca.qc.cgmatane.informatique.marinaconnect.modele.EtreVivant;
+
 public class VueDetailFaune extends AppCompatActivity {
+
+    EtreVivant etreVivant;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_detail_faune);
+        System.out.println(etreVivant.getEspece());
+    }
+
+    public void setEtreVivant(EtreVivant etreVivant){
+        this.etreVivant = etreVivant;
+    }
 }
--- client/app/src/main/java/ca/qc/cgmatane/informatique/marinaconnect/vue/VueListeFaune.java
+++ client/app/src/main/java/ca/qc/cgmatane/informatique/marinaconnect/vue/VueListeFaune.java
@@ -1,5 +1,6 @@
 package ca.qc.cgmatane.informatique.marinaconnect.vue;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
@@ -13,27 +14,36 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.ListIterator;
 
+import ca.qc.cgmatane.informatique.marinaconnect.MarinaConnect;
 import ca.qc.cgmatane.informatique.marinaconnect.R;
 import ca.qc.cgmatane.informatique.marinaconnect.donnee.EtreVivantDAO;
 import ca.qc.cgmatane.informatique.marinaconnect.modele.EtreVivant;
 
 public class VueListeFaune extends AppCompatActivity {
+
     protected EtreVivantDAO accesseurEtreVivant = EtreVivantDAO.getInstance();
     List<EtreVivant> etreVivants;
+
+    static final public int ACTIVITE_DETAIL_FAUNE= 1;
+    protected Intent intentionNaviguerVueDetailFaune;
+
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vue_liste_faune);
+        intentionNaviguerVueDetailFaune = new Intent(this, VueDetailFaune.class);
 
         final ListView listView = (ListView)findViewById(R.id.vue_liste_faune);
         etreVivants = accesseurEtreVivant.listerEtreVivant();
         listView.setAdapter(new CustomListAdapter(this, etreVivants));
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> a, View v, int position, long id) {
                 Object o = listView.getItemAtPosition(position);
                 EtreVivant etreVivant = (EtreVivant) o;
-                System.out.println(etreVivant.getEspece());
                 Toast.makeText(VueListeFaune.this, "Selection :" + " " + etreVivant.getEspece(), Toast.LENGTH_LONG).show();
+
+                startActivityForResult(intentionNaviguerVueDetailFaune, ACTIVITE_DETAIL_FAUNE);
             }
         });
     }
