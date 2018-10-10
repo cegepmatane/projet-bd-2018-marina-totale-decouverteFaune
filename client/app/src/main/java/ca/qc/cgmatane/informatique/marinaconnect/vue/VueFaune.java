17bb67a7d4b9cbca9bd190881f984f946948ccb6
Bassirou <haidara.bassirou@yahoo.com>
2018-10-09 21:24:16 -0400

    #10 Creation vue faune avec maps

---
+++ client/app/src/main/java/ca/qc/cgmatane/informatique/marinaconnect/vue/VueFaune.java
@@ -0,0 +1,15 @@
+package ca.qc.cgmatane.informatique.marinaconnect.vue;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ca.qc.cgmatane.informatique.marinaconnect.R;
+
+public class VueFaune extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_faune);
+    }
+}
