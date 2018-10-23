cd68d1bfec1465b677a50a167ccc005920dbd838
Sedrick <sedrickidd@hotmail.com>
2018-10-23 15:16:12 -0400

    Ajout du service web d'ajout de votes #11

--- serveurDecouverteFaune/src/vote/ajouter/index.php
+++ serveurDecouverteFaune/src/vote/ajouter/index.php
@@ -1,6 +1,24 @@
 <?php
 
 include "../../accesseur/VotesDAO.php";
+$voteDAO = new VotesDAO();
+$cote = $_GET["cote"];
+$idCommentaire = $_GET["idCommentaire"];
+
+$commentaireMoyenne = $voteDAO->recupererMoyenneVote($idCommentaire);
+
+if($commentaireMoyenne == null)
+{
+    $voteDAO->insererUnVote($idCommentaire,$cote);
+}
+else
+{
+  $votes = $voteDAO -> recupererIdVote($idCommentaire);
+  $nouveauTotal = $votes["total_values"] + $cote;
+  $nouveauNombre = $votes["total_votes"] + 1;
+  $voteDAO->mettreAJourUneMoyenne($idCommentaire, $nouveauTotal, $nouveauNombre);
+}
+
 
 
 
