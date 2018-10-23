e3dd8841a5a7e1eb7ba957c66b8f4f36885eb5e3
Bassirou <haidara.bassirou@yahoo.com>
2018-10-23 16:46:14 -0400

    #12 Finalisaton du commentaireDAO

--- client/app/src/main/java/ca/qc/cgmatane/informatique/marinaconnect/donnee/CommentaireDAO.java
+++ client/app/src/main/java/ca/qc/cgmatane/informatique/marinaconnect/donnee/CommentaireDAO.java
@@ -1,5 +1,9 @@
 package ca.qc.cgmatane.informatique.marinaconnect.donnee;
 
+import java.util.concurrent.ExecutionException;
+
+import ca.qc.cgmatane.informatique.marinaconnect.modele.Commentaire;
+
 public class CommentaireDAO {
     private static  CommentaireDAO instance = null;
     protected  CommentaireDAO commentaire;
@@ -10,7 +14,18 @@ public class CommentaireDAO {
         return instance;
     }
 
-    public  void ajouteCommentaireSQL(CommentaireDAO commentaire){
+    public  void ajouteCommentaireSQL(Commentaire commentaire){
 
+
+        try {
+            String url = "http://158.69.113.110/serveurDecouverteFaune/src/commentaire/ajouter/index.php?notecommentaire" + commentaire.getNotecommentaire() + "coordgpscommentaire" + commentaire.getCoordgpscommentaire() + "urlimagecomm" + commentaire.getUrlimagecomm() + "textcom" + commentaire.getTextcom() + "idetrevivant" + commentaire.getIdetrevivant();
+            String resultat;
+            HttpGetRequete getRequete = new HttpGetRequete();
+            resultat = getRequete.execute(url).get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
