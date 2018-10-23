6b12172e0e78bab002ba21af75d5638ce58bd701
Bassirou <haidara.bassirou@yahoo.com>
2018-10-23 15:12:30 -0400

    #12 fonction ajouter un commentaire

--- serveurDecouverteFaune/src/accesseur/CommentaireDAO.php
+++ serveurDecouverteFaune/src/accesseur/CommentaireDAO.php
@@ -10,5 +10,17 @@
 			$requeteListerCommentaire->execute();
 			return $requeteListerCommentaire->fetchAll(PDO::FETCH_OBJ);
 		}
+		 function ajouterCommentaire($commentaire)
+		{
+			$SQL_AJOUTER_COMMENTAIRE = "INSERT into commentaire(notecommentaire,coordgpscommentaire,urlimagecomm,textcom,idetrevivant) VALUES('$commentaire->notecommentaire','$commentaire->coordgpscommentaire','$commentaire->urlimagecomm','$commentaire->textcom','$commentaire->idetrevivant')";
+			
+			global $basededonnees;
+			
+			$requeteAjouterCommentaire = $basededonnees->prepare($SQL_AJOUTER_COMMENTAIRE);
+			$reussite = $requeteAjouterCommentaire->execute();
+			
+			return $reussite;
+			
+		}
 	}
 ?>
