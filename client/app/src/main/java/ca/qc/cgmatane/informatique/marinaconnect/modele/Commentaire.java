56bcb690ba49f49daf7126975d9490bc3164c1b6
Bassirou <haidara.bassirou@yahoo.com>
2018-10-23 15:38:54 -0400

    #9 modele commentaire

---
+++ client/app/src/main/java/ca/qc/cgmatane/informatique/marinaconnect/modele/Commentaire.java
@@ -0,0 +1,51 @@
+package ca.qc.cgmatane.informatique.marinaconnect.modele;
+
+public class Commentaire {
+    protected double notecommentaire;
+    protected int coordgpscommentaire;
+    protected String urlimagecomm;
+    protected String textcom;
+    protected int  idetrevivant;
+
+    public double getNotecommentaire() {
+        return notecommentaire;
+    }
+
+    public void setNotecommentaire(double notecommentaire) {
+        this.notecommentaire = notecommentaire;
+    }
+
+    public int getCoordgpscommentaire() {
+        return coordgpscommentaire;
+    }
+
+    public void setCoordgpscommentaire(int coordgpscommentaire) {
+        this.coordgpscommentaire = coordgpscommentaire;
+    }
+
+    public String getUrlimagecomm() {
+        return urlimagecomm;
+    }
+
+    public void setUrlimagecomm(String urlimagecomm) {
+        this.urlimagecomm = urlimagecomm;
+    }
+
+    public String getTextcom() {
+        return textcom;
+    }
+
+    public void setTextcom(String textcom) {
+        this.textcom = textcom;
+    }
+
+    public int getIdetrevivant() {
+        return idetrevivant;
+    }
+
+    public void setIdetrevivant(int idetrevivant) {
+        this.idetrevivant = idetrevivant;
+    }
+
+
+}
