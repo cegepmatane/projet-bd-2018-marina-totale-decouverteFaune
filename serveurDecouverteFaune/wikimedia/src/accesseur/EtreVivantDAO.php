5c9086da3fd178340977c9dadc42495e0f221d84
Bassirou <haidara.bassirou@yahoo.com>
2018-12-02 12:17:29 -0500

    #13 Wikimedia
    instalation et configuration de l'Api mediawiki

---
+++ serveurDecouverteFaune/wikimedia/src/accesseur/EtreVivantDAO.php
@@ -0,0 +1,23 @@
+<?php
+/*ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+require_once (__DIR__ . '..../vendor/autoload.php' );
+	include_once "baseDeDonnee.php";
+	class EtreVivantDAO
+	{
+		function listerEtreVivant()
+		{			
+			$page = $services->newPageGetter()->getFromTitle( 'Liste des Animaux Marins' );
+		}
+		function ajouterEtreVivant($etreVivant)
+		{			
+			$newContent = new \Mediawiki\DataModel\Content( 'Hello World' );
+			$title = new \Mediawiki\DataModel\Title( 'New Page' );
+			$identifier = new \Mediawiki\DataModel\PageIdentifier( $title );
+			$revision = new \Mediawiki\DataModel\Revision( $newContent, $identifier );
+			$services->newRevisionSaver()->save( $revision );
+		}
+		
+	}*/
+?>
---
+++ serveurDecouverteFaune/wikimedia/src/etreVivant/liste/index.php
@@ -0,0 +1,9 @@
+<?php 
+
+	/*include "../../accesseur/EtreVivantDAO.php";
+	$etreVivantDAO = new EtreVivantDAO();
+	//print_r($etreVivantDAO);
+	$listeEtreVivant = $etreVivantDAO->listerEtreVivant();
+	print_r($listeEtreVivant);*/
+	
+?>
