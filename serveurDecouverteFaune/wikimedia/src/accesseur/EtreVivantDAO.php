117d0ad216fc0f0566f09090aee85140c89bbbc9
Bassirou <haidara.bassirou@yahoo.com>
2018-12-02 12:24:36 -0500

    #5 DAO etreVivant mediawiki
    contient un pti bug

--- serveurDecouverteFaune/wikimedia/src/accesseur/EtreVivantDAO.php
+++ serveurDecouverteFaune/wikimedia/src/accesseur/EtreVivantDAO.php
@@ -1,5 +1,5 @@
 <?php
-/*ini_set('display_errors', 1);
+ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 require_once (__DIR__ . '..../vendor/autoload.php' );
@@ -19,5 +19,5 @@ require_once (__DIR__ . '..../vendor/autoload.php' );
 			$services->newRevisionSaver()->save( $revision );
 		}
 		
-	}*/
+	}
 ?>
