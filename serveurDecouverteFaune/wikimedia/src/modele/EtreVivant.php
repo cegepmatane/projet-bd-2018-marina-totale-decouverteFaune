a3011f084b02a1227a96109e046a86c34dd11907
Bassirou <haidara.bassirou@yahoo.com>
2018-12-05 20:58:44 -0500

    #9 modele etreVivant php

---
+++ serveurDecouverteFaune/wikimedia/src/modele/EtreVivant.php
@@ -0,0 +1,14 @@
+<?php
+class EtreVivant{
+	public  $titre;
+	public  $contenu;
+	function __construct($titre,$contenu)	
+	{
+		$this->titre = $titre;
+		$this->contenu = $contenu;
+	}
+	
+}
+
+
+?>
